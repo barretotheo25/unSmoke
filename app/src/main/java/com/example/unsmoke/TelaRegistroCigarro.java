31bc72a2ff89e5c458e47c5e35d1393c3b95ee4c
theobarretosilva <barretotheo25@gmail.com>
2022-07-15 09:25:55 -0300

    Update do projeto

---
+++ app/src/main/java/com/example/unsmoke/TelaRegistroCigarro.java
@@ -0,0 +1,14 @@
+package com.example.unsmoke;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaRegistroCigarro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_registro_cigarro);
+    }
+}
